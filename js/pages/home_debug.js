f8d5c9c096ff15961c116946241d927638abd313
sagar-ganatra <sagar.shg@gmail.com>
2016-12-14 08:36:56 +0000

    updating site on Wed Dec 14 08:36:56 UTC 2016

---
+++ js/pages/home_debug.js
@@ -0,0 +1,18 @@
+// create a flight component
+var AboutMeBtn = flight.component(aboutMe);
+
+// component definition
+function aboutMe () {
+  // event listener for the click event
+  this.onAboutMeBtnClick = function () {
+    alert('About me');
+  };
+
+  // after the component has been initialized, attched the event listener
+  this.after('initialize', function () {
+    this.on('click', this.onAboutMeBtnClick);
+  });
+}
+
+// attach the component to the DOM node with id - aboutMeBtn
+AboutMeBtn.attachTo('#aboutMeBtn');
