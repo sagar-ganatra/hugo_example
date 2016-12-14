9439bf545092a76e841026ce752555cb17e68af3
System Administrator <root@sagar-2.local>
2016-12-14 13:34:38 +0530

    Adding circle.yml and updated header.html

--- deploy_gh-pages.sh
+++ deploy_gh-pages.sh
@@ -1,6 +1,14 @@
+remote=$(git config remote.origin.url)
+
 rm -rf public
 gulp
 hugo
+
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git init
+git remote add --fetch origin "$remote"
+
 git add -A && git commit -m "updating site on `date`"
 git push origin master
 git subtree push --prefix public origin gh-pages
