7547d473872b6d295b85ba781d53429f2d514ea4
System Administrator <root@sagar-2.local>
2017-01-24 23:05:05 +0530

    Updating the build script

--- deploy_gh-pages.sh
+++ deploy_gh-pages.sh
@@ -8,6 +8,7 @@ git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
+git pull origin master
 
 git add -A && git commit -m "updating site on `date`"
 git push origin master
